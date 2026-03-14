276c2681a35115c099684264bd8953ce000c87c6
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-03-14 12:04:47 -0300

    feat(TicTacToe): Add board

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const ticTacToeGame = (() => {
     return {setDisplay};
   })();
 
+  // A 2D array that represents a 3x3 board
+  const _emptyCell = null;
+  const _board = new Array(3).fill(null).map(() => new Array(3).fill(_emptyCell))
+
   function playTurn() {
 
   };
