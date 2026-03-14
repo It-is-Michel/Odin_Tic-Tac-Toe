931539df1cb6a673c4904556cefa7a32a2bab7b5
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-03-14 11:50:58 -0300

    feat(TicTacToe): Creates abs module's struct with public methods

---
+++ script.js
@@ -0,0 +1,35 @@
+const ticTacToeGame = (() => {
+
+  const _userIntefaceController = (() => {
+    function setDisplay(displayContainer) {
+
+    };
+
+    return {setDisplay};
+  })();
+
+  function playTurn() {
+
+  };
+
+  function resetGame() {
+
+  };
+
+  function getScore() {
+
+  };
+
+  function getBoard() {
+
+  };
+
+  return {
+    setDisplay: _userIntefaceController.setDisplay,
+    playTurn,
+    resetGame,
+    getScore,
+    getBoard
+  };
+
+})();
